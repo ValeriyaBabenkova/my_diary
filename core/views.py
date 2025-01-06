cd401010110b8b2e15a22adc87c2a5d747b60805
ValeriyaBabenkova <vlrbabenkova@gmail.com>
2025-01-06 13:12:14 +0500

    addNotes

--- core/views.py
+++ core/views.py
@@ -3,7 +3,18 @@ from django.http import HttpResponse
 from .models import Note
 
 def notes(request):
+    # получить все записи
     notes = Note.objects.all()
+
+    # добавить новую задачу
+    if request.method == 'POST':
+        date_data = request.POST['date']
+        text_data = request.POST['text']
+
+        if date_data and text_data:
+            Note.objects.create(date=date_data, text=text_data)
+
     return render(request, 'main.html', {'notes': notes})
+
 
 # Create your views here.
