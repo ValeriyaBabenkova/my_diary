6c7f2666a45e047d52140a1974ba175de58b5c22
ValeriyaBabenkova <vlrbabenkova@gmail.com>
2025-02-09 18:09:30 +0500

    addCategoryTry

--- core/views.py
+++ core/views.py
@@ -5,18 +5,30 @@ from .models import Note, NoteCategory
 def notes(request):
     # получить все записи
     notes = Note.objects.all()
+    print(notes)
+
+    category = request.GET.get('category')
+    print(category)
+
+    active_category = None
+    print(active_category)
 
     # добавить новую задачу
     if request.method == 'POST':
-        date_data = request.POST['date']
-        text_data = request.POST['text']
-        category = request.POST.get('category')
-        category = NoteCategory.objects.get(id=category)
+        print(request.POST)
+        date_data = request.POST.get('date')
+        text_data = request.POST.get('text')
+        category = request.POST.get('NoteCategory')
+        active_category = NoteCategory.objects.get(id=category)
+        print(active_category)
 
         if date_data and text_data:
-            Note.objects.create(date=date_data, text=text_data, category=category)
+            Note.objects.create(date=date_data, text=text_data, category=active_category)
 
-    return render(request, 'main.html', {'notes': notes})
+    categories = NoteCategory.objects.all()
+    print(categories)
+
+    return render(request, 'main.html', {'notes': notes, 'categories': categories})
 
 
 # Create your views here.
