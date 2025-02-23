7ce00252c33c339732b84ffade5974b0f7aebfa1
ValeriyaBabenkova <vlrbabenkova@gmail.com>
2025-02-23 11:00:48 +0500

    addLogin

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import *
 
 urlpatterns = [
     path('', notes, name='notes'),
+    path('profiles/', include('profiles.urls'))
 ]
